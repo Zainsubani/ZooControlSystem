0fd98297e69feba6bcfbef87cb781bc9778131f1
Maksim Bogdanov <maxim.bogdanov@protonmail.com>
2018-10-16 02:13:09 +0300

    Dump DB via XML-file

---
+++ src/main/java/hibernate/entity/ZooControlSystemUser.java
@@ -0,0 +1,51 @@
+package hibernate.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ZOO_CONTROL_SYSTEM_USER")
+public class ZooControlSystemUser {
+
+    @Id
+    @Column(name = "LOGIN")
+    private String login;
+
+    @Column(name = "HASH")
+    private byte[] hash;
+
+    @Column(name = "SALT")
+    private byte[] salt;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public byte[] getHash() {
+        return hash;
+    }
+
+    public void setHash(byte[] hash) {
+        this.hash = hash;
+    }
+
+    public byte[] getSalt() {
+        return salt;
+    }
+
+    public void setSalt(byte[] salt) {
+        this.salt = salt;
+    }
+
+    public ZooControlSystemUser(String login, byte[] salt, byte[] hash){
+        this.hash = hash;
+        this.login = login;
+        this.salt = salt;
+    }
+}
